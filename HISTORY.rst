3d4ebf2341511ecbf5cae29c1f9ec87b44a99ad5
Li Ding <lidingpku@gmail.com>
2017-06-19 17:02:14 -0700

    add travis ci config, add history

---
+++ HISTORY.rst
@@ -0,0 +1,11 @@
+.. :changelog:
+
+History
+-------
+
+
+0.1.0 (2017-06-19)
+++++++++++++++++++
+
+* initial PyPI release
+* support json, table(excel), web modules
--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='cdata',
       version='0.0.1',
       description='see data, handy snippets for conversion, cleaning and integration.',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing',
+      ],
       url='http://github.com/cnschema/cdata',
       author='Li Ding',
       author_email='lidingpku@gmail.com',
