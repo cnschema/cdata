20cc521b4515bffc1850b300df97edd69d4eb9e9
Kevin Xin <kevinsxin@126.com>
2017-07-20 10:13:51 +0800

    Added 2 more unit tests, and optimized code into fewer lines

--- cdata/summary.py
+++ cdata/summary.py
@@ -56,23 +56,19 @@ def summarize_entity_person(person):
     if value:
         ret.append(u'祖籍{}'.format(value))
 
-    birth_date = person.get("birthDate")
-    birth_place = person.get("birthPlace")
-    if birth_date and birth_place:
+    birth_date = person.get("birthDate", "")
+    birth_place = person.get("birthPlace", "")
+    if birth_place:
         ret.append(u'{}出生于{}'.format(birth_date, birth_place))
     elif birth_date:
         ret.append(u'{}出生'.format(birth_date))
-    elif birth_place:
-        ret.append(u'出生于{}'.format(birth_place))
 
-    nationality = person.get("nationality")
-    occupation = person.get("occupation")
-    if nationality and occupation:
+    nationality = person.get("nationality", "")
+    occupation = person.get("occupation", "")
+    if occupation:
         ret.append(u'{}{}'.format(nationality, occupation))
     elif nationality:
         ret.append(u'{}人'.format(nationality))
-    elif occupation:
-        ret.append(u'{}'.format(occupation))
 
     value = person.get("authorOf")
     if value:
@@ -83,8 +79,11 @@ def summarize_entity_person(person):
         ret.append( u"主要成就是{}".format(value) )
 
     ret = u"，".join(ret)
-    if ret[-1] != u'。':
-        ret += u'。'
+    ret = re.sub(u"，+", u"，", ret) # Removes repeat commas
+    # Handles periods at end
+    ret = re.sub(u"[。，]+$", u"", ret)
+    ret = ''.join([ret, u"。"])
+
 
     return ret
 
@@ -99,10 +98,10 @@ def task_summarize_entity_person(args):
 
 def task_summarize_all_person(args):
     path2person = file2abspath('../local/person/person.json')
-    person_list = (json.loads(i) for i in file2iter(path2person))
 
     result_person_list = []
-    for person in person_list:
+    for line in file2iter(path2person):
+        person = json.loads(line)
         ret = summarize_entity_person(person)
         if ret:
             person["shortDescription"] = ret
@@ -112,8 +111,17 @@ def task_summarize_all_person(args):
 
     KEYS = [u"@type",u"artName",u"ethnicGroup",u"student",u"courtesyName",u"religion",u"cnProfessionalTitle",u"occupation",u"jobTitle",u"sibling",u"weight",u"nationality",u"birthPlace",u"height",u"alumniOf",u"keywords", u"schoolsOfbuddhism",u"image",u"parent",u"children",u"accomplishment",u"academicDegree",u"dharmaName",u"deathDate",u"academicMajor",u"nobleTitle",u"posthumousName",u"familyName",u"memberOfPoliticalParty",u"award",u"description", u"shortDescription", u"placeOfBurial",u"cnEducationalAttainment",u"alternateName",u"pseudonym",u"templeName",	u"birthDate",u"gender",u"worksFor",u"name",u"dynasty",u"earName",u"ancestralHome",u"birthName",u"studentOf",u"spouse",u"nobleFamily",u"authorOf",u"@id",u"colleague",u"fieldOfWork",u"mother",u"father"]
 
-    json2excel(result_person_list, KEYS, 'person_shortDescription.xls')
-    items2file(result_person_list, 'person_shortDescription.json')
+    out_path = "../local/person/"
+
+    json2excel(
+    result_person_list, KEYS,
+    os.path.join(out_path, 'person_shortDescription.xls')
+    )
+
+    items2file(
+    result_person_list,
+    os.path.join(out_path, 'person_shortDescription.json')
+    )
 
 if __name__ == "__main__":
     logging.basicConfig(format='[%(levelname)s][%(asctime)s][%(module)s][%(funcName)s][%(lineno)s] %(message)s', level=logging.DEBUG)  # noqa
--- tests/test_summary.py
+++ tests/test_summary.py
@@ -25,5 +25,40 @@ class SummaryTestCase(unittest.TestCase):
         ret = summarize_entity_person(person)
         assert u"张三，主要成就是三好学生。" == ret
 
+    def test_real_data(self):
+        person = {
+        "description": u"黄健中，1941年12月29日出生于印度尼西亚泗水，国家一级导演、编剧、艺术指导。1979年，黄健中与张铮联合执导爱情片《小花》，该片获得第三届电影百花奖上获最佳故事片奖 。1982年，黄健中独立执导首部电影作品——爱情片《如意》。1985年，凭借家庭剧《良家妇女》获得第二十五届卡罗维·发利国际电影节主要奖[2-3] 。1990年，拍摄警匪剧《龙年警官》，该片获得第十四届大众电影百花奖最佳故事片奖。1991年，拍摄家庭剧《过年》，该片获得第十五届大众电影百花奖最佳故事片奖。1995年，执导剧情片《大鸿米店》[4-6] 。1998年，拍摄爱情片《红娘》，该片获得第二十二届大众电影百花奖最佳故事片奖[7-8] 。2001年，执导古装武侠剧《笑傲江湖》 。2003年，与佐藤纯弥联合执导家庭犯罪剧《世纪末的晚钟》[10-12] 。2005年，国家广播电影电视总局授予黄健中“优秀电影艺术家”称号 。2006年，执导古装历史剧《越王勾践》 。2009年，拍摄历史战争剧《大秦帝国之裂变》，该片获得第25届中国电视金鹰奖[14-17] 。2011年，执导古装剧《大风歌》[18-19] 。2013年，执导古装神话剧《蓬莱八仙》[20-22] 。",
+        "birthPlace": u"印度尼西亚泗水",
+        "name": u"黄健中",
+        "image": u"http://c.hiphotos.baidu.com/baike/w%3D268%3Bg%3D0/sign=9ac8a3ed33adcbef01347900949449e0/aec379310a55b319a1ae185c41a98226cffc1747.jpg",
+        "accomplishment": u"第4届东京国际电影节评委奖第11届中国电影金鸡奖最佳导演奖第12届中国电影金鸡奖最佳编剧奖",
+        "birthDate": u"1941年12月29日",
+        "keywords": [u"导演", u"娱乐人物", u"人物"],
+        "nationality": u"中国",
+        "alternateName": ["HuangJianzhong", "Huang Jianzhong"],
+        "authorOf": u"过年、龙年警官、越王勾践、大风歌",
+        "@id": u"d67f8dc6-3775-3e4a-9d67-84bb4007d6d1",
+        "@type": ["Person", "Thing"],
+        "occupation": u"导演、编剧、艺术指导，" # Extra comma for punctuation testing
+        }
+        ret = summarize_entity_person(person)
+        assert u"黄健中，1941年12月29日出生于印度尼西亚泗水，中国导演、编剧、艺术指导，主要作品有过年、龙年警官、越王勾践、大风歌。" == ret
+
+        person = {
+        "name": u"陈小群",
+        "gender": u"女",
+        "image": u"http://e.hiphotos.baidu.com/baike/w%3D268%3Bg%3D0/sign=3c89cd72acc379317d68812fd3ffd078/b90e7bec54e736d16b57837c98504fc2d5626979.jpg",
+        "description": u"女，抒情女高音歌唱家，现任上海音乐学院声乐系教授、硕士生导师；先后担任文化部举办的国际声乐比赛全国选拔赛、中国音乐家协会举办的“金钟奖”全国声乐比赛、全国大学生艺术歌曲比赛等比赛评委。",
+        "@type": ["Person", "Thing"],
+        "ethnicGroup": u"汉族",
+        "keywords": [u"音乐", u"行业人物", u"歌手", u"教育", u"娱乐人物", u"人物", u"书籍"],
+        "nationality": u"中国",
+        "@id": u"66548f8a-3f9e-37ca-afb1-e2e96fdb083b",
+        "alumniOf": u"上海音乐学院",
+        "occupation": u"教授"
+        }
+        ret = summarize_entity_person(person)
+        assert u"陈小群，中国教授。" == ret
+
 if __name__ == '__main__':
     unittest.main()
