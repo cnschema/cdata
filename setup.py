a03e498cb5de9fdb8f1c488325fd7067f870a46f
Li Ding <lidingpku@gmail.com>
2017-08-16 17:51:02 -0700

    ready for 0.1.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='cdata',
-      version='0.1.7',
+      version='0.1.8',
       description='see data, handy snippets for conversion, and ETL.',
       long_description=readme(),
       classifiers=[
